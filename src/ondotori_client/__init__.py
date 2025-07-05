9c15b5c3662bb54456de2a8305600165151ecb16
Hiroki Tsusaka <tsusaka4research@gmail.com>
2025-07-05 09:30:06 +0000

    automatic version management added

--- src/ondotori_client/__init__.py
+++ src/ondotori_client/__init__.py
@@ -1,6 +1,12 @@
 # src/ondotori_client/__init__.py
 
-__version__ = "0.3.4"
+from importlib.metadata import version as _pkg_version, PackageNotFoundError as _PkgNotFound
+
+try:
+    __version__ = _pkg_version(__name__.replace("_", "-"))
+except _PkgNotFound:
+    # fallback when package metadata is unavailable (e.g., during source run)
+    __version__ = "0.0.0.dev0"
 
 from .client import OndotoriClient, parse_current, parse_data
 
